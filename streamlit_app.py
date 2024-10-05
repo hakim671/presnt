bd2c256338528a07f3a4ec5319077d2595f15998
hakim671 <boyzodahakim2008@gmail.com>
2024-10-05 23:35:41 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,9 @@
 import streamlit as st
-import pickle
-
-with open(model.pkl, 'rb') as f:
-    model = pickle.load(f)
 
 komn = st.number_input("Количество комнат")
 etag = st.number_input("Этаж")
 plosh = st.number_input("Площадь")
 city = st.selectbox("Выберите город", ["Душанбе", "Худжанд", "Бохтар"])
+tip = st.selectbox("Тип застройки", ["Новостройка", "Вторичный рынок"])
+sost = st.selectbox("Состояние", ["Построено", "На стадии"])
+rem = st.selectbox("Ремонт", ["Новый", "Средний","Без ремонта"])
