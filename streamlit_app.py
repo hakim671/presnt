b2ba0bbb139b27c621027f943b541676a89af204
hakim671 <boyzodahakim2008@gmail.com>
2024-10-05 23:31:53 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 
 with open(model.pkl, 'rb') as f:
-    clf = pickle.load(f)
+    model = pickle.load(f)
 
 komn = st.number_input("Количество комнат")
 etag = st.number_input("Этаж")
