5f86e18beae45c43947678c8855ce201ef44fc2a
hakim671 <boyzodahakim2008@gmail.com>
2024-10-05 23:45:03 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 komn = st.number_input("Количество комнат",value=1)
 etag = st.number_input("Этаж",value=1)
-plosh = st.number_input("Площадь",value=1)
+plosh = st.number_input("Площадь",value=50,step=10)
 city = st.selectbox("Выберите город", ["Душанбе", "Худжанд", "Бохтар"])
 tip = st.selectbox("Тип застройки", ["Новостройка", "Вторичный рынок"])
 sost = st.selectbox("Состояние", ["Построено", "На стадии"])
