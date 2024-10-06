7abe25ce81cd6066479e99745493c86a1d551e15
hakim671 <boyzodahakim2008@gmail.com>
2024-10-06 14:32:07 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
